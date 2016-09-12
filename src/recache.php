1cc209bdd095d5ec2a717276c64c01ccd4fdb331
solohin <solohin.i@gmail.com>
2016-09-12 15:20:41 +0300

    Исправил пути

--- src/recache.php
+++ src/recache.php
@@ -5,6 +5,10 @@
  * Date: 12.09.16
  * Time: 14:19
  */
+if(file_exists(__DIR__.'/../vendor/autoload.php')){
+    require_once __DIR__.'/../vendor/autoload.php';
+}else{
+    require_once __DIR__.'/../../../autoload.php';
+}
 
-require_once __DIR__.'/../vendor/autoload.php';
 $regions = new \solohin\RegionChecker(true, true);
